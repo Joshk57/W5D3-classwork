c4295d500ed78357eb1865a6b0194e169d077068
joshua <joshk.5793@gmail.com>
2023-04-05 11:16:06 -0400

    Added replies and question_likes

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -16,15 +18,57 @@ CREATE TABLE questions (    -- ask about tables connections
 
 CREATE TABLE questions_follows (
     id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL, 
 
-    FOREIGN KEY (user_id) REFERENCES questions(author_id),
+    FOREIGN KEY (author_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
     question_id INTEGER NOT NULL,
-    reply TEXT NOT NULL,
-    FOREIGN KEY (question_id) REFERENCES questions(id)
+    replier_id INTEGER NOT NULL,
+    reply_id INTEGER,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (replier_id) REFERENCES users(id),
+    FOREIGN KEY (reply_id) REFERENCES replies(id)
 );
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_who_liked INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_who_liked) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES quesions(id)
+);
+
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('Anton', 'James'),
+    ('Joshua', 'Kim'),
+    ('Kyle', 'Ginzburg'),
+    ('Ayce','Lacap');
+
+INSERT INTO
+    questions (title, body, author_id)
+VALUES
+    ('Hello', 'How are you?', (SELECT id FROM users WHERE fname = 'Anton' AND lname = 'James')),
+    ('Hi', 'What''s for lunch?', (SELECT id FROM users WHERE fname = 'Joshua' AND lname = 'Kim')),
+    ('Not much', 'Weather is fine', (SELECT id FROM users WHERE fname = 'Anton' AND lname = 'James'));
+
+INSERT INTO
+    questions_follows (author_id, question_id)
+VALUES
+    ((SELECT id FROM users WHERE id = 1), (SELECT id FROM questions WHERE id = 1)),
+    ((SELECT id FROM users WHERE id = 2), (SELECT id FROM questions WHERE id = 2)),
+    ((SELECT id FROM users WHERE id = 1), (SELECT id FROM questions WHERE id = 3));
+
+
+
+
