4039d8372b44fd06889cd7c5e72c25da1c177bf1
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-04-05 10:30:58 -0400

    Create users, questions, follows tables

---
+++ import_db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY, 
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE questions_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL, 
+
+    FOREIGN KEY (user_id) REFERENCES questions(author_id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
